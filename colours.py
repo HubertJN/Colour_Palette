306a3b77d98f42bdd13a9c2affb455f1c69869bd
HubertJN <118004778+HubertJN@users.noreply.github.com>
2024-12-03 17:01:39 +0000

    Update colours.py

--- colours.py
+++ colours.py
@@ -1,4 +1,14 @@
-# colors
-soft_red = "#ed9688"
-soft_blue = "#78b3eb"
-soft_green = "#9befdf"
+colors = {
+    "soft_blue": "#4F81BD",
+    "muted_orange": "#F7A800",
+    "soft_red": "#D14F5D",
+    "gentle_green": "#6DAF69",
+    "soft_purple": "#9B59B6",
+    "dusty_pink": "#E17D85",
+    "slate_blue": "#6A7F99",
+    "warm_tan": "#D3B69B",
+    "earthy_brown": "#8E735B",
+    "moss_green": "#6A8A3B",
+    "rich_gold": "#B88A2A",
+    "charcoal_gray": "#4A4A48"
+}
